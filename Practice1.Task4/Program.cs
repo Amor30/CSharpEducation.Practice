50cc15ea360977b2cc9b45a7814dbb002717cdba
Amor30 <135966233+Amor30@users.noreply.github.com>
2025-05-10 19:08:34 +0400

    Practice 1
    Add all compliting tasks in Practice 1

---
+++ Practice1.Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task4A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первую фразу: ");
+            var firstPhrase = Console.ReadLine();
+
+            Console.Write("Введите первую фразу: ");
+            var secondPhrase = Console.ReadLine();
+
+            Console.WriteLine(firstPhrase + " " + secondPhrase);
+        }
+    }
+}
---
+++ Practice1.Task6/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task6A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstLeg = 3.0;
+            var secLeg = 4.0;
+            var hypotenuse = Math.Sqrt(firstLeg * firstLeg + secLeg * secLeg);
+
+            Console.WriteLine("Известны два катета, 3 и 4, найти гипотинузу. Ответ: {0}", hypotenuse);
+
+            secLeg = Math.Sqrt(hypotenuse * hypotenuse - firstLeg * firstLeg);
+
+            Console.WriteLine("Известен катет 3 и гипотинуза 5, найти второй катет. Ответ {0}", secLeg);
+
+        }
+    }
+}
